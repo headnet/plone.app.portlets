b3b6e843c704caf9021e5ffc3d15615db1778d86
Bo Simonsen <bo@geekworld.dk>
2012-04-04 13:58:03 +0200

    Changes to add portlet metadata for css class

--- plone/app/portlets/manager.py
+++ plone/app/portlets/manager.py
@@ -16,6 +16,10 @@ from plone.portlets.manager import PortletManagerRenderer as BasePortletManagerR
 from plone.app.portlets.interfaces import IColumn
 from plone.app.portlets.interfaces import IDashboard
 #from plone.app.layout.navigation.defaultpage import isDefaultPage
+from plone.memoize.view import memoize
+from plone.portlets.interfaces import IPortletRetriever
+from plone.portlets.utils import hashPortletInfo
+from plone.portlets.interfaces import IPortletAssignmentSettings
 
 logger = logging.getLogger('portlets')
 
@@ -23,6 +27,42 @@ logger = logging.getLogger('portlets')
 class PortletManagerRenderer(BasePortletManagerRenderer, Explicit):
     """A Zope 2 implementation of the default PortletManagerRenderer
     """
+
+    @memoize
+    def _lazyLoadPortlets(self, manager):
+        retriever = getMultiAdapter((self.context, manager), IPortletRetriever)
+        items = []
+        for p in self.filter(retriever.getPortlets()):
+            renderer = self._dataToPortlet(p['assignment'].data)
+            info = p.copy()
+            info['manager'] = self.manager.__name__
+            info['renderer'] = renderer
+            hashPortletInfo(info)
+            # Record metadata on the renderer
+            renderer.__portlet_metadata__ = info.copy()
+            del renderer.__portlet_metadata__['renderer']
+            try:
+                isAvailable = renderer.available
+            except ConflictError:
+                raise
+            except Exception, e:
+                isAvailable = False
+                logger.exception(
+                    "Error while determining renderer availability of portlet "
+                    "(%r %r %r): %s" % (
+                    p['category'], p['key'], p['name'], str(e)))
+
+            info['available'] = isAvailable
+
+            assignments = info['assignment'].__parent__
+            settings = IPortletAssignmentSettings(assignments[info['name']])
+            info['settings'] = settings
+
+            items.append(info)
+
+        return items
+
+
 
     def _dataToPortlet(self, data):
         """Helper method to get the correct IPortletRenderer for the given
